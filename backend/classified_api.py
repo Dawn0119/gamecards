76064b36f61b0900b567a58be6fafc8a4dcdb472
YaksaX000 <ma41sv@gmail.com>
2025-06-09 11:53:17 +0800

    color model update

--- backend/classified_api.py
+++ backend/classified_api.py
@@ -3,7 +3,7 @@ import requests
 # --- Roboflow API Config ---
 API_KEY = "jPtq6WNi8tOUWrx5we1t"
 
-MODEL_ID = "color-zagok/3"
+MODEL_ID = "color-zagok/4"
 CONFIDENCE = 0.18
 
 def get_card_class(image_path):
